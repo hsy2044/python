c8b6023272f523108004ae7b7e49a397752f4e71
Joseph <Joseph_Zhuo@syscom.com.tw>
2023-12-25 08:43:46 +0800

    取消

--- fep-web/src/main/java/com/syscom/fep/web/form/common/UI_080030_FormDetail.java
+++
@@ -1,9 +0,0 @@
-package com.syscom.fep.web.form.common;
-
-import com.syscom.fep.web.form.BaseForm;
-
-public class UI_080030_FormDetail extends BaseForm {
-    private static final long serialVersionUID = 1L;
-
-
-}
