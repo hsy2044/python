0ec280e534976f0725cd335611912eb856d7e5e8
M5-494207-NB-01\User <Vivian_Huang@syscom.com.tw>
2024-01-05 14:06:42 +0800

    調整

--- test/src/main/java/org/example/log.java
+++ test/src/main/java/org/example/log.java
@@ -15,7 +15,7 @@ public class log {
             // 使用BufferedReader來讀取檔案
             BufferedReader bufferedReader = new BufferedReader(new FileReader(txtFilePath));
             Scanner scanner = new Scanner(System.in);
-            System.out.println("請輸入:");
+            System.out.println("FSCODE:");
             String num = scanner.nextLine();
             // 讀取每一行的內容
             String line;
@@ -23,16 +23,13 @@ public class log {
 
                 String[] k = line.split("\\|");
 
-                if(k[6].trim().equals(num))
+                if(k[4].trim().contains(num))
                 {
-                    if(k[9].trim().equals("AdapterIn")||k[9].trim().equals("AdapterOut"))
+                    if(k[9].trim().equals("AAIn")||k[9].trim().equals("AdapterIn")||k[9].trim().equals("AdapterOut")||k[9].trim().equals("AAOut"))
                     {
                         System.out.println(k[0]+k[2]+" "+k[4]+k[6]+k[9]+k[18]);
                     }
-
                 }
-
-
             }
 
             // 關閉資源
