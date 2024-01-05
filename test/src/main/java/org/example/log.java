b22abb109b77d65df9cab813408d1db045c2213a
M5-494207-NB-01\User <Vivian_Huang@syscom.com.tw>
2024-01-05 10:36:43 +0800

    小廢log程式

--- test/src/main/java/org/example/log.java
+++ test/src/main/java/org/example/log.java
@@ -1,4 +1,44 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class log {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            // 指定txt檔案的路徑
+            String txtFilePath = "C:\\Users\\User\\Desktop\\LOG\\fep-server-atm-ATMService-2023-02-23-0.log";
+
+            // 使用BufferedReader來讀取檔案
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(txtFilePath));
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("請輸入:");
+            String num = scanner.nextLine();
+            // 讀取每一行的內容
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+
+                String[] k = line.split("\\|");
+
+                if(k[6].trim().equals(num))
+                {
+                    if(k[9].trim().equals("AdapterIn")||k[9].trim().equals("AdapterOut"))
+                    {
+                        System.out.println(k[0]+k[2]+" "+k[4]+k[6]+k[9]+k[18]);
+                    }
+
+                }
+
+
+            }
+
+            // 關閉資源
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
